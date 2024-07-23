c25ba53e07163916e92d8e6f2e939f238ebca165
“nemevidplus” <63751839+nemevid@users.noreply.github.com>
2024-07-23 12:22:25 +0200

    Update client folder with recent changes

--- client/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override, addWebpackPlugin } = require('customize-cra');
-const webpack = require('webpack');
-
-module.exports = override(
-  addWebpackPlugin(new webpack.DefinePlugin({
-    'process.env.NODE_OPTIONS': JSON.stringify('--openssl-legacy-provider')
-  }))
-);
